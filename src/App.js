f8cf64b7690b07e84a915527dec1d02d25814b46
manonab <abelcoindozm@gmail.com>
2019-05-27 17:42:52 +0200

    my work of the forms and the button for the yoship project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Login from './components/Login';
+import Button from './components/Button';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      {/* <Register /> */}
+      </header>
+
+      <body>
+      {/* <Login /> */}
+      <Register /> 
+      </body>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.css';
+
+class Button extends React.Component {
+
+    render() {
+        return (
+        <div>
+            <p>
+                <button className="login-button">SUBMIT</button> 
+            </p>
+        </div>
+        )
+    }
+}
+
+export default Button;
